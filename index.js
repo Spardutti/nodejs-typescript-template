7be4327c2b80773f41d8060babfa6f0fc405cdb7
spardutti <luisdamian.sp@gmail.com>
2022-08-27 13:35:48 -0300

    linter

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get("/", (req, res) => {
-    res.send("Express + Ts Server");
+    res.send("Express + Ts Server Template");
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at ${port}`);
