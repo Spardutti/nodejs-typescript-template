29524e2b2bf7625fbc39afb9cb0e70a0227a6ff5
spardutti <luisdamian.sp@gmail.com>
2022-08-27 13:49:18 -0300

    linter

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -9,8 +11,8 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.get("/", (req, res) => {
-    res.send("Express + Ts Server Template");
+  res.send("Express + Ts Server");
 });
 app.listen(port, () => {
-    console.log(`[server]: Server is running at ${port}`);
+  console.log(`[server]: Server is running at this port  ${port}`);
 });
